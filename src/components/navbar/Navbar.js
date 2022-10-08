1fd591d9c52ede0853fa7755f686a6e3676181ee
devumairazmat <umairazmat8260@gmail.com>
2022-10-08 09:03:54 +0500

    navbar done

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <>
+      {/* Nav wrapper */}
+      <div className="n-wrapper">
+        {/* Nav left */}
+        <div className="n-left">
+          <div className="n-name">Umair</div>
+          <span>toggle</span>
+        </div>
+        {/* Nav Right */}
+        <div className="n-right">
+          <div className="n-list">
+            <ul style={{ listStyleType: "none" }}>
+              <li>Home</li>
+              <li>Services</li>
+              <li>Experience</li>
+              <li>Pertfolio</li>
+              <li>Testimonial</li>
+            </ul>
+          </div>
+          <button className="button n-button">Contact Me</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
